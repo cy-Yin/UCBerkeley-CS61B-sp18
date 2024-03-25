a092adca4c628639a2962900fa16dee92794f8b0
yinchaoyang <YCY2857959697@hotmail.com>
2024-03-25 13:56:18 +0800

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,18 @@
  */
 public class LeapYear {
 
+    /** Determine whether the given year is a leap year.
+     *  @param year to be analyzed 
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
