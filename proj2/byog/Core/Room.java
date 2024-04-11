98fd31ed4590b07e0ef98e89e184d0064b1a847b
yinchaoyang <YCY2857959697@hotmail.com>
2024-04-11 12:06:56 +0800

    complete proj2 phase1. third try AG

--- proj2/byog/Core/Room.java
+++ proj2/byog/Core/Room.java
@@ -57,7 +57,7 @@ public class Room {
     public Position innerPosRandom(MapGenerationParameters mgp) {
         Random random = new Random(mgp.seed);
         int innerX = pos.x + 1 + RandomUtils.uniform(random, width - 2);  // from 1 to width - 2
-        int innerY = pos.y + 1 + RandomUtils.uniform(random,height - 2); // from 1 to height - 2
+        int innerY = pos.y + 1 + RandomUtils.uniform(random, height - 2); // from 1 to height - 2
         return new Position(innerX, innerY);
     }
 }
