305047f08b8b45895a6328ebbf53a47277d94601
yinchaoyang <YCY2857959697@hotmail.com>
2024-04-30 20:27:18 +0800

    complete proj3 part I. first try AG

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,9 +8,69 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    private final int maxDepth = 7;
 
     public Rasterer() {
-        // YOUR CODE HERE
+
+    }
+
+    /** Returns true if the params is valid, false otherwise.
+     *  "valid" means:
+     *  1. the params must contain all the information for query,
+     *      including the upper-left and lower-left longitudes/latitudes of the query box
+     *      and the width and height of user's viewpoint.
+     *  2. the query box must have a valid area, which means the upper-left corner must be
+     *      at the northwest direction of the lower-left one.
+     *  3. the query box is not allowed to be completely outside the root longitude/latitudes,
+     *      which means it must contain part of the root map at least.
+     *
+     * @param params Map of the HTTP GET request's query parameters - the query box and
+     *              the user viewport width and height.
+     */
+    private boolean isValidParams(Map<String, Double> params) {
+        if (!(params.containsKey("lrlon") && params.containsKey("ullon")
+            && params.containsKey("w") && params.containsKey("h")
+            && params.containsKey("ullat") && params.containsKey("lrlat"))) {
+            return false;
+        }
+
+        /* For a valid query box
+         *
+         * ulLat -> ***********
+         *          *         *
+         *          *         *
+         * lrLat -> ***********
+         *          ^         ^
+         *        ulLon     lrLon
+         *
+         * ulLat > lrLat and ulLon < lrLon
+         */
+        double lrLon = params.get("lrlon");
+        double ulLon = params.get("ullon");
+        double lrLat = params.get("lrlat");
+        double ulLat = params.get("ullat");
+
+        return lrLon > ulLon && lrLat < ulLat // query box with a valid area
+                // query box is not allowed to be completely outside the root longitude/latitudes
+                && ulLat > MapServer.ROOT_LRLAT && lrLat < MapServer.ROOT_ULLAT
+                && ulLon < MapServer.ROOT_LRLON && lrLon > MapServer.ROOT_ULLON;
+    }
+
+    /** Returns the appropriate depth of images.
+     * the map with an appropriate depth has a resolution
+     * which is just equal to or greater than the demanded one.
+     *
+     * @param demandLonDPP the demanded LonDPP calculated through the user's query params
+     */
+    private int getDepth(double demandLonDPP) {
+        int depth = 0;
+        // initialize LonDPP to d0.
+        double depthLonDPP = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / MapServer.TILE_SIZE;
+        while (depth < maxDepth && depthLonDPP > demandLonDPP) {
+            depthLonDPP = depthLonDPP / 2;
+            depth = depth + 1;
+        }
+        return depth;
     }
 
     /**
@@ -44,9 +104,115 @@ public class Rasterer {
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+        if (!isValidParams(params)) {
+            results.put("render_grid", null);
+            results.put("raster_ul_lon", null);
+            results.put("raster_ul_lat", null);
+            results.put("raster_lr_lon", null);
+            results.put("raster_lr_lat", null);
+            results.put("depth", null);
+            results.put("query_success", false);
+            return results;
+        }
+
+        double lrLon = params.get("lrlon");
+        double ulLon = params.get("ullon");
+        double width = params.get("w");
+        double height = params.get("h");
+        double ulLat = params.get("ullat");
+        double lrLat = params.get("lrlat");
+
+        double demandLonDPP = (lrLon - ulLon) / width;
+        int depth = getDepth(demandLonDPP);
+
+        int[] corners = getRenderGridCorners(depth, lrLon, ulLon, ulLat, lrLat);
+        int startX = corners[0];
+        int endX = corners[1];
+        int startY = corners[2];
+        int endY = corners[3];
+
+        String[][] renderGrid = new String[endY - startY + 1][endX - startX + 1];
+        for (int i = startX; i <= endX; i += 1) {
+            for (int j = startY; j <= endY; j += 1) {
+                renderGrid[j - startY][i - startX] = "d" + depth
+                                    + "_" + "x" + i + "_" + "y" + j + ".png";
+            }
+        }
+
+        double rasterULLon = MapServer.ROOT_ULLON
+                + startX * (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / Math.pow(2, depth);
+        double rasterULLat = MapServer.ROOT_ULLAT
+                - startY * (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2, depth);
+        double rasterLRLon = MapServer.ROOT_ULLON
+                + (endX + 1) * (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / Math.pow(2, depth);
+        double rasterLRLat = MapServer.ROOT_ULLAT
+                - (endY + 1) * (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2, depth);
+
+        results.put("render_grid", renderGrid);
+        results.put("raster_ul_lon", rasterULLon);
+        results.put("raster_ul_lat", rasterULLat);
+        results.put("raster_lr_lon", rasterLRLon);
+        results.put("raster_lr_lat", rasterLRLat);
+        results.put("depth", depth);
+        results.put("query_success", true);
         return results;
+    }
+
+    /** Returns an array containing the corners' positions of the grid.
+     *  To be more specific, returns [startX, endX, startY, endY]
+     *
+     * @param depth the depth of the map with appropriate resolution
+     * @param lrLon longitude of the lower-right corner
+     * @param ulLon longitude of the upper-left corner
+     * @param ulLat latitude of the upper-left corner
+     * @param lrLat latitude of the lower-right corner
+     */
+    private int[] getRenderGridCorners(int depth, double lrLon, double ulLon,
+                                                double ulLat, double lrLat) {
+        double numImagesN = Math.pow(2, depth); // totally N * N images on the root map
+        double stepX = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / numImagesN;
+        double stepY = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / numImagesN;
+
+        // calculates the coordinates of images need to be displayed
+        int startX = 0;
+        int startY = 0;
+        int endX = 0;
+        int endY = 0;
+
+        final double x = MapServer.ROOT_ULLON;
+        while (x + startX * stepX <= ulLon && x + startX * stepX <= MapServer.ROOT_LRLON) {
+            startX += 1;
+        }
+        if (startX > 0) {
+            startX -= 1;
+        }
+        while (x + endX * stepX <= lrLon && x + endX * stepX <= MapServer.ROOT_LRLON
+                                                                    && endX < numImagesN) {
+            endX += 1;
+        }
+        if (endX > 0) {
+            endX -= 1;
+        }
+
+        final double y = MapServer.ROOT_ULLAT;
+        while (y - startY * stepY >= ulLat && y - startY * stepY >= MapServer.ROOT_LRLAT) {
+            startY += 1;
+        }
+        if (startY > 0) {
+            startY -= 1;
+        }
+        while (y - endY * stepY >= lrLat && y - endY * stepY >= MapServer.ROOT_LRLAT
+                                                                    && endY < numImagesN) {
+            endY += 1;
+        }
+        if (endY > 0) {
+            endY -= 1;
+        }
+
+
+        int[] corners = {startX, endX, startY, endY};
+        return corners;
     }
 
 }
